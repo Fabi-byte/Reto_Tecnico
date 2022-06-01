34692d47fd3828409df25b8e3d7d0484fcd92e46
Fabi-byte <f.kerbesa@gmail.com>
2022-06-01 09:36:04 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,13 @@
-output "s3_bucket_name" {
+output "s3_bucket_private_name" {
   value = aws_s3_bucket.my-s3-private-bucket.id
 }
-output "s3_bucket_region" {
+output "s3_bucket_private_region" {
     value = aws_s3_bucket.my-s3-private-bucket.region
 }
 
-output "s3_bucket_name" {
+output "s3_bucket_public_name" {
   value = aws_s3_bucket.my-s3-public-bucket.id
 }
-output "s3_bucket_region" {
+output "s3_bucket_public_region" {
     value = aws_s3_bucket.my-s3-public-bucket.region
 }
