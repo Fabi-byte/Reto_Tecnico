548e3d26b47bcd27bbf1b51f357ea65333ca19a7
Fabi-byte <74878590+Fabi-byte@users.noreply.github.com>
2022-05-31 21:55:59 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,85 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+
+cloud {
+    organization = "fabi-byte"
+
+    workspaces {
+      name = "Prueba_Tecnica"
+    }
+  }
+
+
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+  access_key = "AKIAR5YNTKTOIPSR5E6H"
+  secret_key = "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"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_s3_bucket" "my-s3-private-bucket" {
+  bucket_prefix = var.bucket_priv_prefix
+  acl = "private"
+  
+   versioning {
+    enabled = var.versioning
+  }
+  
+  tags = var.tags
+}
+
+resource "aws_s3_bucket" "my-s3-public-bucket" {
+  bucket_prefix = var.bucket_pub_prefix
+  acl = var.acl
+  
+   versioning {
+    enabled = var.versioning
+  }
+  
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_object" "cat1" {
+  bucket = "acg-testlab-public-20220531041204156600000001"
+  key    = "cat1.jpg"
+  source = "Files/cat2.jpg"
+  acl = "public-read"
+  
+}
+
+
+resource "aws_s3_bucket_object" "priv_cat1" {
+  bucket = "acg-testlab-private-20220531041204156600000002"
+  key    = "cat1.jpg"
+  source = "Files/cat1.jpg"
+  
+}
+
+
+
+
+
+resource "github_repository" "Prueba_tecnica" {
+  name        = "Prueba_Tecnica"
+  description = "Repositorio para prueba tecnica de terraform"
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+
+variable "bucket_priv_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+    default     = "acg-testlab-private-"
+}
+variable "bucket_pub_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+    default     = "acg-testlab-public-"
+}
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+variable "acl" {            
+    type        = string
+    description = " Defaults to public "
+    default     = "public-read"
+}
+
