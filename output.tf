38d1f62ce8507363a66df676da5dc7091e6865ab
Fabi-byte <f.kerbesa@gmail.com>
2022-06-01 09:34:32 -0500

    New keys
    none

--- output.tf
+++ output.tf
@@ -4,3 +4,10 @@ output "s3_bucket_name" {
 output "s3_bucket_region" {
     value = aws_s3_bucket.my-s3-private-bucket.region
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my-s3-public-bucket.id
+}
+output "s3_bucket_region" {
+    value = aws_s3_bucket.my-s3-public-bucket.region
+}
